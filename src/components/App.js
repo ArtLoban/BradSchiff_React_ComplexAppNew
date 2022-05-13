d440511a513fd6046c73ceb7d58d5dcba636ede9
Artem <artem.loban@devit-team.com>
2022-05-13 16:45:48 +0300

    App. Base structure

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import react from 'react';
+
+/* Components */
+import Header from './Header';
+import HomeGuest from './HomeGuest';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <HomeGuest />
+      <Footer />
+    </>
+  )
+}
+
+export default App;
